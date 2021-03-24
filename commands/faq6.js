ec43190cbe931b158322b8d1163d9656de51fa93
StoneMcYT <Stone.mc.buissness@gmail.com>
2021-03-24 21:13:07 +0800

    More FAQ files

--- commands/faq6.js
+++ commands/faq6.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
 module.exports = {
-  name: 'faq2',
-  description: 'The 2nd faq page',
+  name: 'faq6',
+  description: 'The 6th faq page',
   execute(message){
-const faq1 = new Discord.MessageEmbed()
+const faq5 = new Discord.MessageEmbed()
 .setColor('#00FFFF')
 .setAuthor(`${message.author.username}`, `${message.author.displayAvatarURL({ dynamic: true })}`)
 .setTitle ('Faq')
     .addFields( 
-      {name:"2. How do I become a Verified Builder?", value: "https://buildtheearth.net/bte-asean"},
+      {name:"5. How do I get Youtube roles in the ASEAN Server?", value: "Please, ask the staffs of this server or <@724495837325164576>"},
       )
    .setFooter('Last updated on 22/3/2021','https://images-ext-1.discordapp.net/external/j2-U816cNP_653mZv8L7hc1Q6TN6D9NhahEx5QMjPYs/%3Fsize%3D128/https/cdn.discordapp.com/icons/702883639574396969/0a9741c68d97fd00de8458d20fd9b513.png');
-   message.channel.send(faq1);
+   message.channel.send(faq5);
 }
 }
